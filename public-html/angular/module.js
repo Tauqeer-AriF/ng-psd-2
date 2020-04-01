d0406aa17e6c9daf79cbc81d77d2bf6c10b5c73b
Tauqeer-AriF <chtauqeer1117@gmail.com>
2020-04-01 09:22:23 -0700

    angular psd 2

---
+++ public-html/angular/module.js
@@ -0,0 +1,10 @@
+var a= angular.module('1st',[]);
+  
+a.controller('b', function($scope){
+
+   $scope.logo = "images/Notify.@1X.png";
+   $scope.hand = "images/Composed-layer@1X.png";
+   $scope.editable = "Editable Theme"
+
+
+});
